961a5a66299a60ccaf7c76416850d1a34a83db74
n0op3 <n0op3@proton.me>
2025-11-06 18:31:59 +0100

    suggestions

--- src/main/java/com/github/n0op3/mixin/WorldGenScreenMixin.java
+++ src/main/java/com/github/n0op3/mixin/WorldGenScreenMixin.java
@@ -1,19 +1,17 @@
 package com.github.n0op3.mixin;
 
 import com.github.n0op3.BetterWorldPresets;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.client.gui.screen.world.WorldCreationSettings;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(CreateWorldScreen.class)
 public abstract class WorldGenScreenMixin extends Screen {
@@ -22,14 +20,12 @@ public abstract class WorldGenScreenMixin extends Screen {
         super(title);
     }
 
-    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/DirectionalLayoutWidget;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;"), locals = LocalCapture.CAPTURE_FAILHARD,
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/DirectionalLayoutWidget;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;"),
             slice = @Slice(
                     from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/DirectionalLayoutWidget;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;"),
                     to = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/DirectionalLayoutWidget;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;")
             ))
-    private void init(CallbackInfo ci, DirectionalLayoutWidget directionalLayoutWidget) {
-        this.addDrawableChild(directionalLayoutWidget.add(ButtonWidget.builder(Text.literal("Test button"), button -> {
-            BetterWorldPresets.LOGGER.info("HELLO WORLD!!!");
-        }).build()));
+    private void init(CallbackInfo ci, @Local DirectionalLayoutWidget directionalLayoutWidget) {
+        this.addDrawableChild(directionalLayoutWidget.add(ButtonWidget.builder(Text.literal("Test button"), button -> BetterWorldPresets.LOGGER.info("HELLO WORLD!!!")).build()));
     }
 }
