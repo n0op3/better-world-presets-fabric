0aefb38aa234166c18fe6f9d87c56cc8940fd256
n0op3 <n0op3@proton.me>
2025-11-06 19:15:29 +0100

    icons and proper buttons

--- src/main/java/com/github/n0op3/mixin/WorldGenScreenMixin.java
+++ src/main/java/com/github/n0op3/mixin/WorldGenScreenMixin.java
@@ -3,9 +3,10 @@ package com.github.n0op3.mixin;
 import com.github.n0op3.BetterWorldPresets;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -17,10 +18,10 @@ public abstract class WorldGenScreenMixin extends Screen {
         super(title);
     }
 
-    @ModifyVariable(method = "init", at =  @At("STORE"), ordinal = 0)
+    @ModifyVariable(method = "init", at = @At("STORE"), ordinal = 0)
     private DirectionalLayoutWidget injected(DirectionalLayoutWidget directionalLayoutWidget) {
-        this.addDrawableChild(directionalLayoutWidget.add(ButtonWidget.builder(Text.literal("L"), button -> BetterWorldPresets.LOGGER.info("Load a preset!")).width(20).build()));
-        this.addDrawableChild(directionalLayoutWidget.add(ButtonWidget.builder(Text.literal("S"), button -> BetterWorldPresets.LOGGER.info("Save a preset!")).width(20).build()));
+        this.addDrawableChild(directionalLayoutWidget.add(TextIconButtonWidget.builder(Text.empty(), button -> BetterWorldPresets.LOGGER.info("Export a preset"), true).width(20).texture(Identifier.of(BetterWorldPresets.MOD_ID, "icon/export"), 15, 15).build()));
+        this.addDrawableChild(directionalLayoutWidget.add(TextIconButtonWidget.builder(Text.empty(), button -> BetterWorldPresets.LOGGER.info("Import a preset"), true).width(20).texture(Identifier.of(BetterWorldPresets.MOD_ID, "icon/import"), 15, 15).build()));
         return directionalLayoutWidget;
     }
 }
