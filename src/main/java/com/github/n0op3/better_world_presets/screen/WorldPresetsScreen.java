383a0169d9b05f247d7adc973b09b1851de6a50e
n0op3 <n0op3@proton.me>
2025-11-07 18:14:43 +0100

    use the grid and fix init

--- src/main/java/com/github/n0op3/better_world_presets/screen/WorldPresetsScreen.java
+++ src/main/java/com/github/n0op3/better_world_presets/screen/WorldPresetsScreen.java
@@ -1,10 +1,7 @@
 package com.github.n0op3.better_world_presets.screen;
 
-import com.github.n0op3.better_world_presets.BetterWorldPresets;
 import com.github.n0op3.better_world_presets.widget.PresetListWidget;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.RenderPipelines;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.*;
 import net.minecraft.text.Text;
@@ -12,7 +9,7 @@ import net.minecraft.text.Text;
 public class WorldPresetsScreen extends Screen {
 
     private final Screen parent;
-    private final ThreePartsLayoutWidget layout;
+    private ThreePartsLayoutWidget layout;
 
     public WorldPresetsScreen() {
         super(Text.literal("World Presets"));
@@ -22,25 +19,18 @@ public class WorldPresetsScreen extends Screen {
 
     @Override
     protected void init() {
+        this.clearChildren();
+        this.layout = new ThreePartsLayoutWidget(this, 33, 54);
         layout.addHeader(new TextWidget(Text.literal("Preset Menu"), MinecraftClient.getInstance().textRenderer));
 
         layout.addBody(new PresetListWidget(MinecraftClient.getInstance(), layout.getWidth(), layout.getContentHeight(), layout.getY() + layout.getHeaderHeight(), layout.getHeaderHeight()));
 
-        DirectionalLayoutWidget layoutOfLayouts = layout.addFooter(DirectionalLayoutWidget.vertical().spacing(8));
+        GridWidget grid = layout.addFooter(new GridWidget(0, 0).setRowSpacing(4).setColumnSpacing(8));
 
-        DirectionalLayoutWidget directionalLayout = DirectionalLayoutWidget.horizontal().spacing(8);
-        directionalLayout.add(ButtonWidget.builder(Text.literal("Create new preset"), button -> {}).build());
-        directionalLayout.add(ButtonWidget.builder(Text.literal("Edit preset"), button -> {}).build());
-
-        directionalLayout.refreshPositions();
-        layoutOfLayouts.add(directionalLayout);
-
-        DirectionalLayoutWidget directionalLayout2 = DirectionalLayoutWidget.horizontal().spacing(8);
-        directionalLayout2.add(ButtonWidget.builder(Text.literal("Delete preset"), button -> {}).build());
-        directionalLayout2.add(ButtonWidget.builder(Text.literal("Load preset"), button -> {}).build());
-
-        directionalLayout2.refreshPositions();
-        layoutOfLayouts.add(directionalLayout2);
+        grid.add(ButtonWidget.builder(Text.literal("Create new preset"), button -> {}).build(), 0, 0);
+        grid.add(ButtonWidget.builder(Text.literal("Edit preset"), button -> {}).build(), 0, 1);
+        grid.add(ButtonWidget.builder(Text.literal("Delete preset"), button -> {}).build(), 1, 0);
+        grid.add(ButtonWidget.builder(Text.literal("Load preset"), button -> {}).build(), 1, 1);
 
         layout.refreshPositions();
         layout.forEachChild(this::addDrawableChild);
