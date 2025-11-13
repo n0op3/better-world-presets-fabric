7a126bf75430f5583a3c73d06fa7586f4d819ed4
n0op3 <n0op3@proton.me>
2025-11-13 16:15:50 +0100

    turn the record to a class to allow modifications

--- src/main/java/com/github/n0op3/better_world_presets/BetterWorldPreset.java
+++ src/main/java/com/github/n0op3/better_world_presets/BetterWorldPreset.java
@@ -5,16 +5,72 @@ import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 
-public record BetterWorldPreset(
-        String worldName,
-        String seed,
-        boolean generateStructures,
-        boolean bonusChest,
-        WorldCreator.Mode gameMode,
-        Difficulty difficulty,
-        boolean commandsAllowed,
-        GameRules gameRules,
-        ChunkGenerator chunkGenerator,
-        WorldCreator.WorldType worldType
-) {
+public class BetterWorldPreset {
+    private String worldName;
+    private final String seed;
+    private final boolean generateStructures;
+    private final boolean bonusChest;
+    private final WorldCreator.Mode gameMode;
+    private final Difficulty difficulty;
+    private final boolean commandsAllowed;
+    private final GameRules gameRules;
+    private final ChunkGenerator chunkGenerator;
+    private final WorldCreator.WorldType worldType;
+
+    public BetterWorldPreset(String worldName, String seed, boolean generateStructures, boolean bonusChest, WorldCreator.Mode gameMode, Difficulty difficulty, boolean commandsAllowed, GameRules gameRules, ChunkGenerator chunkGenerator, WorldCreator.WorldType worldType) {
+        this.worldName = worldName;
+        this.seed = seed;
+        this.generateStructures = generateStructures;
+        this.bonusChest = bonusChest;
+        this.gameMode = gameMode;
+        this.difficulty = difficulty;
+        this.commandsAllowed = commandsAllowed;
+        this.gameRules = gameRules;
+        this.chunkGenerator = chunkGenerator;
+        this.worldType = worldType;
+    }
+
+    public String worldName() {
+        return worldName;
+    }
+
+    public void setWorldName(String worldName) {
+        this.worldName = worldName;
+    }
+
+    public String seed() {
+        return seed;
+    }
+
+    public boolean generateStructures() {
+        return generateStructures;
+    }
+
+    public boolean bonusChest() {
+        return bonusChest;
+    }
+
+    public WorldCreator.Mode gameMode() {
+        return gameMode;
+    }
+
+    public Difficulty difficulty() {
+        return difficulty;
+    }
+
+    public boolean commandsAllowed() {
+        return commandsAllowed;
+    }
+
+    public GameRules gameRules() {
+        return gameRules;
+    }
+
+    public ChunkGenerator chunkGenerator() {
+        return chunkGenerator;
+    }
+
+    public WorldCreator.WorldType worldType() {
+        return worldType;
+    }
 }
